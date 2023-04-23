ae7b9c9055676b480e1c6e16ce6beddaebb70075
Phong Van Ngo <1952034@gmail.com>
2023-04-23 23:23:03 +0700

    fix to set cookie successfully

--- packages/medusa/src/api/middlewares/authenticate.ts
+++ packages/medusa/src/api/middlewares/authenticate.ts
@@ -1,12 +1,30 @@
 import { NextFunction, Request, RequestHandler, Response } from "express"
 import passport from "passport"
+import jwt from "jsonwebtoken"
 
 export default (): RequestHandler => {
   return (req: Request, res: Response, next: NextFunction): void => {
-    passport.authenticate(["admin-jwt", "bearer"], { session: false })(
-      req,
-      res,
-      next
-    )
+    const {
+      projectConfig: { jwt_secret },
+    } = req.scope.resolve("configModule")
+
+    console.log("cookie: ",req.cookies.novapo);
+    console.log("jwt: ",jwt_secret);
+
+    const token = req.cookies.novapo;
+    jwt.verify(token, jwt_secret, (err, decoded) => {
+      if (err) {
+        res.status(401).json({ error: 'Unauthorized' });
+      } else {
+        req.user = decoded;
+        next();
+      }
+    });
+
+    // passport.authenticate(["admin-jwt", "bearer"], { session: false })(
+    //   req,
+    //   res,
+    //   next
+    // )
   }
 }
